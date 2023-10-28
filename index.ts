175b6a05a2fc65c270d85b19e3454880c9f80c61
AnshVM <anshmalik2002@gmail.com>
2023-10-28 13:07:57 +0530

    use from cli

--- index.ts
+++ index.ts
@@ -1,3 +1,27 @@
+import { readFileSync } from "fs";
 import { Analyzer } from "./engine";
-import axios from "axios";
+
+
+if (process.argv.length == 2) {
+    console.log("Usage: bun index.ts [filenames]")
+} else {
+
+    for (let i = 2; i < process.argv.length; i++) {
+        const filename = process.argv[i]
+        if (!filename.endsWith('.js')) {
+            console.log(`Invalid file: ${filename}. Only .js files are supported.`)
+            continue;
+        }
+        try {
+            const source = readFileSync(filename).toString()
+            const analyzer = new Analyzer(source)
+            console.log("")
+            console.log(filename)
+            analyzer.run()
+        } catch (error) {
+            console.log("Error while reading file: ", error)
+        }
+    }
+
+}
 
